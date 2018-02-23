d5c56e617bb96edc7e08426bd366d93aeaaf77f4
chai2010 <chaishushan@gmail.com>
2018-02-24 07:12:18 +0800

    add package doc

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2018 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache-style
+// license that can be found in the LICENSE file.
+
+// Package libconfd provides mini confd lib.
+package libconfd
