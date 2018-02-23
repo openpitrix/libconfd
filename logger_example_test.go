59e43e8b9f57911b5a5faa9c5dd00699ea1094d6
chai2010 <chaishushan@gmail.com>
2018-02-24 07:44:20 +0800

    add logger example code

---
+++ logger_example_test.go
@@ -0,0 +1,16 @@
+// Copyright 2018 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache-style
+// license that can be found in the LICENSE file.
+
+package libconfd
+
+func ExampleLogger() {
+	var logger Logger = new(Glogger)
+
+	logger.Info("hello")
+	logger.Warning("confd")
+
+	if logger.V(1) {
+		logger.Info("debug: ...")
+	}
+}
