e0b4eee9eeac08bb3c7a295b8ac18e22777aac45
chai2010 <chaishushan@gmail.com>
2018-02-23 15:48:17 +0800

    fix logger copyright

--- logger.go
+++ logger.go
@@ -1,6 +1,6 @@
-// Copyright confd. All rights reserved.
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE-confd file.
+// Copyright 2018 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a Apache-style
+// license that can be found in the LICENSE file.
 
 package libconfd
 
