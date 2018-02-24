c6fe82904355f31882adefea5200b6c784268858
chai2010 <chaishushan@gmail.com>
2018-02-24 11:46:59 +0800

    refine fileStat for posix

--- util_fileStat_posix.go
+++ util_fileStat_posix.go
@@ -17,20 +17,32 @@ import (
 
 // fileStat return a fileInfo describing the named file.
 func fileStat(name string) (fi fileInfo, err error) {
-	if isFileExist(name) {
-		f, err := os.Open(name)
-		if err != nil {
-			return fi, err
-		}
-		defer f.Close()
-		stats, _ := f.Stat()
-		fi.Uid = stats.Sys().(*syscall.Stat_t).Uid
-		fi.Gid = stats.Sys().(*syscall.Stat_t).Gid
-		fi.Mode = stats.Mode()
-		h := md5.New()
-		io.Copy(h, f)
-		fi.Md5 = fmt.Sprintf("%x", h.Sum(nil))
-		return fi, nil
+	if !isFileExist(name) {
+		err = errors.New("File not found")
+		return
 	}
-	return fi, errors.New("File not found")
+
+	f, err := os.Open(name)
+	if err != nil {
+		return fi, err
+	}
+	defer f.Close()
+
+	stats, err := f.Stat()
+	if err != nil {
+		return
+	}
+
+	fi.Uid = stats.Sys().(*syscall.Stat_t).Uid
+	fi.Gid = stats.Sys().(*syscall.Stat_t).Gid
+	fi.Mode = stats.Mode()
+
+	h := md5.New()
+	_, err = io.Copy(h, f)
+	if err != nil {
+		return
+	}
+
+	fi.Md5 = fmt.Sprintf("%x", h.Sum(nil))
+	return fi, nil
 }
