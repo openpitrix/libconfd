b71d0d7de8244c345001002d590868968fe6dbe4
chai2010 <chaishushan@gmail.com>
2018-02-28 23:05:52 +0800

    refine Config helper funcs

--- config.go
+++ config.go
@@ -4,6 +4,14 @@
 
 package libconfd
 
+import (
+	"bytes"
+	"encoding/gob"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
 type Config struct {
 	ConfDir       string
 	ConfigDir     string
@@ -13,4 +21,60 @@ type Config struct {
 	SyncOnly      bool
 	TemplateDir   string
 	PGPPrivateKey []byte
+}
+
+func MustLoadConfig(name string) Config {
+	p, err := LoadConfig(name)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	return p
+}
+
+func LoadConfig(name string) (p Config, err error) {
+	md, err := toml.DecodeFile(name, p)
+	if err != nil {
+		return Config{}, err
+	}
+	if unknownKeys := md.Undecoded(); len(unknownKeys) != 0 {
+		logger.Warning("config: Undecoded keys:", unknownKeys)
+	}
+	return p, nil
+}
+
+func (p *Config) Save(name string) error {
+	buf := new(bytes.Buffer)
+	if err := toml.NewEncoder(buf).Encode(p); err != nil {
+		return err
+	}
+
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(buf.String()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *Config) Clone() Config {
+	var (
+		q   Config
+		buf bytes.Buffer
+	)
+
+	enc := gob.NewEncoder(&buf)
+	dec := gob.NewDecoder(&buf)
+
+	if err := enc.Encode(p); err != nil {
+		logger.Fatal(err)
+	}
+	if err := dec.Decode(&q); err != nil {
+		logger.Fatal(err)
+	}
+
+	return q
 }
