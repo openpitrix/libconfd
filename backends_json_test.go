f39adb1360994632b05c3dffb2cfb6e748da8a47
chai2010 <chaishushan@gmail.com>
2018-03-04 09:11:02 +0800

    simplify json backend test

--- backends_json_test.go
+++ backends_json_test.go
@@ -5,21 +5,11 @@
 package libconfd
 
 import (
-	"io/ioutil"
-	"os"
 	"testing"
 )
 
 func TestJsonBackend(t *testing.T) {
-	os.MkdirAll("./_temp", 0766)
-
-	jsonFile := "./_temp/backend-json-file.json"
-	err := ioutil.WriteFile(jsonFile, []byte(tJsonFileContent), 0666)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	c := NewJsonBackendClient(jsonFile)
+	c := NewJsonBackendClient("./testadta/simple.json")
 	m, err := c.GetValues([]string{""})
 	if err != nil {
 		t.Fatal(err)
@@ -29,22 +19,3 @@ func TestJsonBackend(t *testing.T) {
 		t.Fatal(v)
 	}
 }
-
-var tJsonFileContent = `
-{
-    "/key": "foobar",
-    "/database/host": "127.0.0.1",
-    "/database/password": "p@sSw0rd",
-    "/database/port": "3306",
-    "/database/username": "libconfd",
-    "/upstream/app1": "10.0.1.10:8080",
-    "/upstream/app2": "10.0.1.11:8080",
-    "/prefix/database/host": "127.0.0.1",
-    "/prefix/database/password": "p@sSw0rd",
-    "/prefix/database/port": "3306",
-    "/prefix/database/username": "libconfd",
-    "/prefix/upstream/app1": "10.0.1.10:8080",
-    "/prefix/upstream/app2": "10.0.1.11:8080",
-    "-": ""
-}
-`
