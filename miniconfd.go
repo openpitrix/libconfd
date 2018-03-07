1289c6344ec5cef3bf2f1087eff139d30ffea833
chai2010 <chaishushan@gmail.com>
2018-03-08 01:18:53 +0800

    reinfe miniconfd

--- miniconfd.go
+++ miniconfd.go
@@ -13,22 +13,14 @@ import (
 )
 
 var (
-	flagWatch         = flag.Bool("watch", false, "use watch mode")
-	flagOnetime       = flag.Bool("onetime", false, "run once and exit")
-	flagInterval      = flag.Int("interval", 10, "backend polling interval seconds")
-	flagConfdir       = flag.String("confdir", "", "confd conf directory")
-	flagConfigFile    = flag.String("config-file", "./testdata/confd.toml", "the confd config file")
-	flagJsonFile      = flag.String("file", "./testdata/simple.json", "the JSON file to watch for changes")
-	flagKeepStageFile = flag.Bool("file", false, "keep staged files")
-	flagLogLevel      = flag.String("log-level", "INFO", "log level. oneof: DEBUG/INFO/WARN/ERROR/PANIC")
-	flagNoop          = flag.Bool("noop", false, "only show pending changes")
+	cfgfile = flag.String("config-file", "./confd.toml", "config file")
 )
 
 func main() {
 	flag.Parse()
 
-	cfg := libconfd.MustLoadConfig(*flagConfigFile)
-	client := libconfd.NewJsonBackendClient(*flagJsonFile)
+	cfg := libconfd.MustLoadConfig(*cfgfile)
+	client := libconfd.NewFileBackendsClient(cfg.File)
 
 	libconfd.NewProcessor(cfg, client).Run()
 }
