2ac869a986412fbb920a055ce52c34de459ef37d
chai2010 <chaishushan@gmail.com>
2018-03-13 17:42:36 +0800

    refacting processor

--- processor.go
+++ processor.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE-confd file.
 
+// Package libconfd provides mini confd lib.
 package libconfd
 
 import (
@@ -9,8 +10,6 @@ import (
 	"sync"
 	"time"
 )
-
-var ErrShutdown = errors.New("libconfd: processor is shut down")
 
 type Call struct {
 	Config Config
@@ -39,18 +38,6 @@ type Processor struct {
 	wg        sync.WaitGroup
 }
 
-func NewProcessor() *Processor {
-	logger.Debugln(getFuncName())
-
-	p := &Processor{
-		closeChan: make(chan bool),
-	}
-
-	p.wg.Add(1)
-	go p.run()
-	return p
-}
-
 func (p *Processor) isClosing() bool {
 	if p.closeChan == nil {
 		panic("closeChan is nil")
@@ -69,7 +56,6 @@ func (p *Processor) pushPendingCall(call *Call) {
 
 	p.pending = append(p.pending, call)
 }
-
 func (p *Processor) popPendingCall() *Call {
 	p.pendingMutex.Lock()
 	defer p.pendingMutex.Unlock()
@@ -82,23 +68,48 @@ func (p *Processor) popPendingCall() *Call {
 	p.pending = p.pending[:len(p.pending)-1]
 	return call
 }
-
 func (p *Processor) clearPendingCall() {
 	p.pendingMutex.Lock()
 	defer p.pendingMutex.Unlock()
 
 	for _, call := range p.pending {
-		call.Error = ErrShutdown
+		call.Error = errors.New("libconfd: processor is shut down")
 		call.done()
 	}
 
 	p.pending = p.pending[:0]
 }
 
-func (p *Processor) Close() error {
-	close(p.closeChan)
-	p.wg.Wait()
-	return nil
+func NewProcessor() *Processor {
+	p := &Processor{
+		closeChan: make(chan bool),
+	}
+
+	p.wg.Add(1)
+	go func() {
+		defer p.wg.Done()
+
+		for {
+			if p.isClosing() {
+				p.clearPendingCall()
+				return
+			}
+
+			call := p.popPendingCall()
+			if call == nil {
+				time.Sleep(time.Second / 10)
+				continue
+			}
+
+			p.wg.Add(1)
+			go func() {
+				defer p.wg.Done()
+				p.process(call)
+			}()
+		}
+	}()
+
+	return p
 }
 
 func (p *Processor) Go(cfg Config, client Client, opts ...Options) *Call {
@@ -109,7 +120,7 @@ func (p *Processor) Go(cfg Config, client Client, opts ...Options) *Call {
 	call.Opts = append([]Options{}, opts...)
 	call.Done = make(chan *Call, 10) // buffered.
 
-	p.send(call)
+	p.pushPendingCall(call)
 	return call
 }
 
@@ -118,35 +129,137 @@ func (p *Processor) Run(cfg Config, client Client, opts ...Options) error {
 	return call.Error
 }
 
-func (p *Processor) send(call *Call) {
-	if p.isClosing() {
-		call.Error = ErrShutdown
-		call.done()
+func (p *Processor) Close() error {
+	close(p.closeChan)
+	p.wg.Wait()
+	return nil
+}
+
+func (p *Processor) process(call *Call) {
+	opt := newOptions(call.Opts...)
+
+	switch {
+	case opt.useOnetimeMode:
+		p.runOnce(call)
+	case opt.useIntervalMode:
+		p.runInIntervalMode(call)
+	case opt.useWatchMode:
+		p.runInWatchMode(call)
+	default:
+		if call.Client.WatchEnabled() {
+			p.runInWatchMode(call)
+		} else {
+			p.runInIntervalMode(call)
+		}
+	}
+}
+
+func (p *Processor) runOnce(call *Call) {
+	opt := newOptions(call.Opts...)
+
+	ts, err := MakeAllTemplateResourceProcessor(call.Config, call.Client)
+	if err != nil {
+		logger.Error(err)
 		return
 	}
 
-	p.pushPendingCall(call)
-}
-
-func (p *Processor) run() {
-	defer p.wg.Done()
-
-	for {
-		if p.isClosing() {
-			p.clearPendingCall()
+	var allErrors []error
+	for _, t := range ts {
+		if p.isClosing() || opt.isClosing() {
 			return
 		}
 
-		call := p.popPendingCall()
-		if call == nil {
-			time.Sleep(time.Second / 10)
+		if err := t.Process(call.Opts...); err != nil {
+			allErrors = append(allErrors, err)
+			logger.Error(err)
+		}
+	}
+
+	return
+}
+
+func (p *Processor) runInIntervalMode(call *Call) {
+	opt := newOptions(call.Opts...)
+
+	for {
+		if p.isClosing() || opt.isClosing() {
+			return
+		}
+
+		ts, err := MakeAllTemplateResourceProcessor(call.Config, call.Client)
+		if err != nil {
+			logger.Warning(err)
 			continue
 		}
 
-		p.wg.Add(1)
-		go func() {
-			defer p.wg.Done()
-			p.process(call)
-		}()
+		for _, t := range ts {
+			if p.isClosing() {
+				return
+			}
+
+			if err := t.Process(call.Opts...); err != nil {
+				logger.Error(err)
+			}
+		}
+
+		time.Sleep(opt.GetInterval())
+	}
+}
+
+func (p *Processor) runInWatchMode(call *Call) {
+	opt := newOptions(call.Opts...)
+
+	ts, err := MakeAllTemplateResourceProcessor(call.Config, call.Client)
+	if err != nil {
+		logger.Warning(err)
+		return
+	}
+
+	var wg sync.WaitGroup
+	var stopChan = make(chan bool)
+
+	for i := 0; i < len(ts); i++ {
+		wg.Add(1)
+		go func(t *TemplateResourceProcessor) {
+			defer wg.Done()
+			p.monitorPrefix(t, &wg, stopChan, call)
+		}(ts[i])
+	}
+
+	for {
+		time.Sleep(time.Second / 2)
+
+		if p.isClosing() || opt.isClosing() {
+			close(stopChan)
+			break
+		}
+	}
+
+	wg.Wait()
+	return
+}
+
+func (p *Processor) monitorPrefix(
+	t *TemplateResourceProcessor,
+	wg *sync.WaitGroup, stopChan chan bool,
+	call *Call,
+) {
+	opt := newOptions(call.Opts...)
+	keys := t.getAbsKeys()
+
+	for {
+		if p.isClosing() || opt.isClosing() {
+			return
+		}
+
+		index, err := t.client.WatchPrefix(t.Prefix, keys, t.lastIndex, stopChan)
+		if err != nil {
+			logger.Error(err)
+		}
+
+		t.lastIndex = index
+		if err := t.Process(call.Opts...); err != nil {
+			logger.Error(err)
+		}
 	}
 }
